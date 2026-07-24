17288ab6a838344436460a4a8072f16d44acee0b
ekobadd <ekobaddish@gmail.com>
2026-07-24 08:57:25 -0400

    Reset animations on save

--- Scripts/CloudGeneratorManager.gd
+++ Scripts/CloudGeneratorManager.gd
@@ -6,6 +6,8 @@ extends Node
 # Cloud velocity in pixels per second.
 @export var cloud_vel : Vector2 = Vector2(-10, 0)
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- Scripts/PlayDefaultTool.gd
+++ Scripts/PlayDefaultTool.gd
@@ -5,3 +5,10 @@ extends AnimatedSprite2D
 
 func _enter_tree() -> void:
 	play(animation_name)
+
+func _notification(what: int) -> void:
+	if Engine.is_editor_hint():
+		if what == NOTIFICATION_EDITOR_PRE_SAVE:
+			stop()
+		elif what == NOTIFICATION_EDITOR_POST_SAVE:
+			play(animation_name)
